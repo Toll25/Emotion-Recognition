e7c215bea7e0b984fbd8c9c1c030e4d8752f6919
Toll <paulipanther.hoesch@gmail.com>
2024-01-25 16:22:50 +0100

    pfusch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import cv2
-import numpy as np
-from fer import FER
 import threading
+import cv2
+import numpy
+import os
+from deepface import DeepFace
 
 cam = cv2.VideoCapture(0)
 
@@ -17,68 +18,104 @@ current_img = None
 
 # Define the list to store multiple rectangle coordinates
 rectangle_list = []
-emotions_list = []
+data_list = []
+
+# It helps in identifying the faces
+
+size = 4
+haar_file = 'haarcascade_frontalface_default.xml'
+datasets = 'datasets'
+
+# Part 1: Create fisherRecognizer
+print('Recognizing Face Please Be in sufficient Lights...')
+
+# Create a list of images and a list of corresponding names
+(images, labels, names, id) = ([], [], {}, 0)
+for (subdirs, dirs, files) in os.walk(datasets):
+    for subdir in dirs:
+        names[id] = subdir
+        subjectpath = os.path.join(datasets, subdir)
+        for filename in os.listdir(subjectpath):
+            path = subjectpath + '/' + filename
+            label = id
+            images.append(cv2.imread(path, 0))
+            labels.append(int(label))
+        id += 1
+(width, height) = (130, 100)
+
+# Create a Numpy array from the two lists above
+(images, labels) = [numpy.array(lis) for lis in [images, labels]]
+
+# OpenCV trains a model from the images
+# NOTE FOR OpenCV2: remove '.face'
+model = cv2.face.LBPHFaceRecognizer_create()
+model.train(images, labels)
+
+# Part 2: Use fisherRecognizer on camera stream
+face_cascade = cv2.CascadeClassifier(haar_file)
 
 
-# Function to draw rectangles on the image
-def draw_reference_id(image, rectangle, emotion, id):
-    if rectangle:
-        x, y, _, _ = rectangle
-        reference_id_text = f"Ref ID: {id}"  # You can replace 123 with your reference ID
-        cv2.putText(image, reference_id_text, (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        cv2.putText(image, emotion, (x, y - 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+def draw_rectangles_and_emotions(image, data, faces):
+    for face, (x, y, w, h) in zip(data, faces):
+        region = face.get("region")
+        box_x = region.get("x")
+        box_y = region.get("y")
+        box_height = region.get("h")
+        box_width = region.get("w")
+        cv2.rectangle(image, (box_x, box_y), (box_x + box_width, box_y + box_height), (255, 0, 0), 2)
+        gender = face.get("dominant_gender")
+        emotion = face.get("dominant_emotion")
+        name = "not Recognized"
+
+        gender_percent = convertFloatToIntString(face.get("gender").get(gender))
+        emotion_percent = convertFloatToIntString(face.get("emotion").get(emotion))
+
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        # Try to recognize the face
+        prediction = model.predict(face_resize)
+        if prediction[1] < 95:
+            name = '% s - %.0f' % (names[prediction[0]], prediction[1])
+
+        # Draw reference ID on the main frame with the highest emotion
+        cv2.putText(image, "Name: " + name, (x, y - 30), cv2.QT_FONT_NORMAL, 0.5, (255, 0, 0), 2)
+        cv2.putText(image, "Emotion: " + emotion + " (" + emotion_percent + "%)",
+                    (x, y - 50), cv2.QT_FONT_NORMAL, 0.5, (255, 0, 0), 2)
+        cv2.putText(image, "Gender: " + gender + " (" + gender_percent + "%)", (x, y - 70), cv2.QT_FONT_NORMAL,
+                    0.5, (255, 0, 0), 2)
+
+        # Define the function that will run in a separate thread
 
 
-def draw_rectangles_and_emotions(image, rectangles, emotions):
-    id=0
-    for rect, emotion_dict in zip(rectangles, emotions):
-        x, y, width, height = rect
-        cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 2)
-
-        # Sort emotions by score in descending order
-        sorted_emotions = sorted(emotion_dict.items(), key=lambda x: x[1], reverse=True)
-
-        # Display emotions near the rectangle in a separate window
-        emotions_window = np.zeros((len(sorted_emotions) * 30, 200, 3), dtype=np.uint8)
-        for i, (emotion, score) in enumerate(sorted_emotions):
-            # Format the score as a percentage
-            score_percentage = score * 100
-            text = f"{emotion.upper()}: {score_percentage:.0f}%"
-            cv2.putText(emotions_window, text, (10, i * 30 + 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-            # Check if this emotion is the highest
-            if i == 0:
-                highest_emotion = emotion
-                # Draw reference ID on the main frame with the highest emotion
-                draw_reference_id(image, rect, highest_emotion, id)
-
-        cv2.imshow("Emotions", emotions_window)
+def convertFloatToIntString(floatValue):
+    return str(numpy.int8(numpy.rint(floatValue)))
 
 
-# Define the function that will run in a separate thread
 def emotion_thread():
     global current_img
     global rectangle_list
-    global emotions_list
+    global data_list
     while True:
         # Acquire the lock to safely access the current_img variable
         with lock:
             img_copy = current_img.copy() if current_img is not None else None
 
         if img_copy is not None:
-            detector = FER(mtcnn=True)
-            result = detector.detect_emotions(img_copy)
+            cv2.imwrite("img.png", img_copy)
+            deep_results = DeepFace.analyze(img_path="img.png",
+                                            actions=['gender', 'emotion'],
+                                            enforce_detection=False
+                                            )
 
             # Clear the existing rectangle list
-            rectangle_list = []
-            emotions_list = []
+            data_list = []
 
-            if result:
-                for results in result:
-                    if "box" in results and len(results["box"]) == 4:
-                        x, y, width, height = results["box"]
-                        rectangle_list.append((x, y, width, height))
-                        emotions_list.append(results["emotions"])
+            if deep_results:
+                for result in deep_results:
+                    if result.get("face_confidence") == 0:
+                        print("No Face")
+                    else:
+                        data_list = deep_results
 
 
 # Start the new thread
@@ -96,12 +133,16 @@ while True:
 
     image_with_rectangles = img.copy()
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    data_faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
     # Draw rectangles and display emotions on the image if there is at least one entry
-    if rectangle_list:
-        draw_rectangles_and_emotions(image_with_rectangles, rectangle_list, emotions_list)
+    if data_list:
+        draw_rectangles_and_emotions(image_with_rectangles, data_list, data_faces)
 
     cv2.imshow("Webcam", image_with_rectangles)
-    if cv2.waitKey(10) & 0xFF == 27:
+    key = cv2.waitKey(10)
+    if key == 27:
         break
 
 # Release the camera and close all windows
--- testscript.py
+++
@@ -1,8 +0,0 @@
-import time
-
-import tensorflow as tf
-
-print(tf.test.gpu_device_name())
-
-while True:
-    time.sleep(1)
